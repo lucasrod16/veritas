461eb8bd9eaed4866d05521766d8b9fd9a2722ef
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2024-08-17 04:22:20 -0500

    simplify scan
    Signed-off-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -9,9 +9,6 @@ import (
 	"github.com/anchore/grype/grype"
 	"github.com/anchore/grype/grype/db"
 	"github.com/anchore/grype/grype/matcher"
-	"github.com/anchore/grype/grype/matcher/golang"
-	"github.com/anchore/grype/grype/matcher/java"
-	"github.com/anchore/grype/grype/matcher/stock"
 	"github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/presenter/cyclonedx"
 	"github.com/anchore/grype/grype/presenter/models"
@@ -21,28 +18,41 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-const mavenSearchBaseURL = "https://search.maven.org/solrsearch/select"
+const grypeDBListingURL = "https://toolbox-data.anchore.io/grype/databases/listing.json"
+
+var grypeDBdir = filepath.Join(xdg.CacheHome, "veritas", "db")
 
 func Scan(userInput string) (string, error) {
-	var g errgroup.Group
-	var err error
+	var (
+		err error
+		g   errgroup.Group
 
-	var store *store.Store
-	var status *db.Status
-	var closer *db.Closer
+		store  *store.Store
+		status *db.Status
+		closer *db.Closer
+
+		packages   []pkg.Package
+		pkgContext pkg.Context
+		sbom       *sbom.SBOM
+	)
+
 	g.Go(func() error {
-		store, status, closer, err = grype.LoadVulnerabilityDB(newGrypeDBCfg(), true)
+		store, status, closer, err = grype.LoadVulnerabilityDB(db.Config{
+			DBRootDir:  grypeDBdir,
+			ListingURL: grypeDBListingURL,
+		}, true)
 		if err != nil {
 			return err
 		}
 		return nil
 	})
 
-	var packages []pkg.Package
-	var pkgContext pkg.Context
-	var sbom *sbom.SBOM
 	g.Go(func() error {
-		packages, pkgContext, sbom, err = pkg.Provide(userInput, getProviderConfig())
+		packages, pkgContext, sbom, err = pkg.Provide(userInput, pkg.ProviderConfig{
+			SyftProviderConfig: pkg.SyftProviderConfig{
+				SBOMOptions: syft.DefaultCreateSBOMConfig(),
+			},
+		})
 		if err != nil {
 			return fmt.Errorf("failed to catalog: %w", err)
 		}
@@ -58,62 +68,27 @@ func Scan(userInput string) (string, error) {
 
 	vulnMatcher := grype.VulnerabilityMatcher{
 		Store:    *store,
-		Matchers: getMatchers(),
+		Matchers: matcher.NewDefaultMatchers(matcher.Config{}),
 	}
 
-	remainingMatches, _, err := vulnMatcher.FindMatches(packages, pkgContext)
+	matches, _, err := vulnMatcher.FindMatches(packages, pkgContext)
 	if err != nil {
 		return "", err
 	}
 
-	presenterCfg := models.PresenterConfig{
-		Matches:          *remainingMatches,
+	pres := cyclonedx.NewJSONPresenter(models.PresenterConfig{
+		Matches:          *matches,
 		Packages:         packages,
 		Context:          pkgContext,
 		MetadataProvider: store,
 		SBOM:             sbom,
 		DBStatus:         status,
-	}
+	})
 
-	pres := cyclonedx.NewJSONPresenter(presenterCfg)
 	buf := &bytes.Buffer{}
 	err = pres.Present(buf)
 	if err != nil {
 		return "", err
 	}
-
 	return buf.String(), nil
-}
-
-func newGrypeDBCfg() db.Config {
-	return db.Config{
-		DBRootDir:  filepath.Join(xdg.CacheHome, "veritas", "db"),
-		ListingURL: "https://toolbox-data.anchore.io/grype/databases/listing.json",
-	}
-}
-
-func getProviderConfig() pkg.ProviderConfig {
-	return pkg.ProviderConfig{
-		SyftProviderConfig: pkg.SyftProviderConfig{
-			SBOMOptions: syft.DefaultCreateSBOMConfig(),
-		},
-	}
-}
-
-func getMatchers() []matcher.Matcher {
-	return matcher.NewDefaultMatchers(
-		matcher.Config{
-			Golang: golang.MatcherConfig{
-				AlwaysUseCPEForStdlib:                  true,
-				AllowMainModulePseudoVersionComparison: false,
-			},
-			Java: java.MatcherConfig{
-				ExternalSearchConfig: java.ExternalSearchConfig{
-					SearchMavenUpstream: true,
-					MavenBaseURL:        mavenSearchBaseURL,
-				},
-			},
-			Stock: stock.MatcherConfig{UseCPEs: true},
-		},
-	)
 }
