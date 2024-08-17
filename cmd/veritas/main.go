78f036f3469d4dba69e50e76ac6f3a7c99667ab6
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2024-08-17 03:25:01 -0500

    do not git ignore cmd/veritas
    Signed-off-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

---
+++ cmd/veritas/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/lucasrod16/veritas/veritashttp"
+)
+
+func main() {
+	shutdownSignal := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignal, os.Interrupt)
+
+	err := veritashttp.StartServer(context.Background(), shutdownSignal)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
