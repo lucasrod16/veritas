9e354e5b34f1e154e578bf1d2fd9047b19cd5d45
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2024-08-17 17:50:18 -0500

    set json resp header
    Signed-off-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -26,6 +26,7 @@ func scanHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.Write([]byte(reportPayload))
 		return
 	}
